9b224aaad4a466e7cc5256c4b87dcc4a7a1dcf56
Lukas <Lukas.Erekson@gmail.com>
2022-01-10 21:51:18 -0700

    Change get request for /articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
 
-  get 'articles/index', to: 'articles#index'
+  get '/articles', to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
