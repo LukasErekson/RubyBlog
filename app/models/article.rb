7a5c6b9d3a9c82e62e00cd96de4e58ffabe635f7
Lukas <Lukas.Erekson@gmail.com>
2022-01-11 20:57:03 -0700

    Validate title and body for new articles
    - Display errors to the user upon a failed save

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 72 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
